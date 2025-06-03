a923bcad43488acedc1fbea10b570f8711ad07aa
jamesfulreader <jamesfulreader@gmail.com>
2025-06-03 09:53:49 -0600

    codemod used to remove legacybehavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
             <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
               className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
+              legacyBehavior>
               {session ? "Sign out" : "Sign in"}
             </Link>
 
