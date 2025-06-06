c5013508cc60ab9bf11c1ac294508f02f11c2993
jamesfulreader <jamesfulreader@gmail.com>
2025-06-06 11:23:15 -0600

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    env: {
+        AUTH_SECRET: process.env.AUTH_SECRET,
+        AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+        AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+        DATABASE_URL: process.env.POSTGRES_URL_NON_POOLING,
+        NODE_ENV: process.env.NODE_ENV,
+    },
+};
 
 export default config;
