f9424684b72e822519d332b74d32b2e07c791a5a
jamesfulreader <jamesfulreader@gmail.com>
2025-06-09 10:36:09 -0600

    remove posts from app and drop table from db

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { websiteRouter } from "~/server/api/routers/website";
 import { communicationLogRouter } from "~/server/api/routers/communicationLog";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
@@ -9,7 +8,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   website: websiteRouter,
   communicationLog: communicationLogRouter,
 });
